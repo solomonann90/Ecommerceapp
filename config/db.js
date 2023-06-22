f26f3605500b297aa2e5840a0d8d1ccc1ff92005
Ann Solomon <solomonann90@gmail.com>
2023-06-22 02:04:01 -0400

    Final Code
    MERN Ecommerce App

---
+++ config/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import colors from "colors";
+import morgan from "morgan";
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URL);
+    console.log(
+      `Conneted To Mongodb Database ${conn.connection.host}`.bgMagenta.white
+    );
+  } catch (error) {
+    console.log(`Errro in Mongodb ${error}`.bgRed.white);
+  }
+};
+
+export default connectDB;
---
+++ server.js
@@ -0,0 +1,48 @@
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import cors from "cors";
+import authRoutes from "./routes/authRoute.js"
+import categoryRoutes from "./routes/categoryRoutes.js"
+import productRoutes from "./routes/productRoutes.js"
+import path from 'path'
+
+
+//configure env
+dotenv.config();
+
+
+//databse config
+connectDB()
+
+//rest object
+const app = express();
+
+//middelwares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(express.static(path.join__dirname, './client/build'))
+
+//routes
+app.use("/api/auth", authRoutes)
+app.use("/api/category", categoryRoutes)
+app.use("/api/product", productRoutes)
+
+//rest api
+app.use('*', function (req, res) {
+    res.sendFile(path.join__dirname, './client/build/index.html')
+})
+
+//PORT
+const PORT = process.env.PORT;
+
+//run listen
+app.listen(PORT, () => {
+    console.log(
+        `Server Running on  port ${PORT}`.bgCyan.white
+
+    );
+});
