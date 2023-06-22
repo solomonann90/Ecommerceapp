4536aab7b2e8a57fbfe6fa73f03b1a673bc8d3cc
solomonann90 <135493566+solomonann90@users.noreply.github.com>
2023-06-22 12:26:33 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import authRoutes from "./routes/authRoute.js"
 import categoryRoutes from "./routes/categoryRoutes.js"
 import productRoutes from "./routes/productRoutes.js"
 import path from 'path'
+import fileURLToPath from 'url'
 
 
 //configure env
@@ -16,6 +17,10 @@ dotenv.config();
 
 //databse config
 connectDB()
+
+//esmodulefix
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 //rest object
 const app = express();
